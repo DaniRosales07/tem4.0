997207c987efb0e43034f8bbfa73534756f5bd58
Daniel Rosales <realgraf.ok@gmail.com>
2024-07-30 19:14:12 -0300

    arreglo sec diseños

--- src/pages-jsx/Diseños.jsx
+++ src/pages-jsx/Diseños.jsx
@@ -14,13 +14,13 @@ const Diseños = () => {
         <div className="grid1 espacios-verticales">
             <div className=" centrar-textos">
             <Link target="_blank" to="https://heyzine.com/flip-book/e0208e40bc.html">
-            <img className="imagengrande" src="https://firebasestorage.googleapis.com/v0/b/trabajosenmendoza-9801f.appspot.com/o/f1.png?alt=media&token=6629a1a2-ed13-40f9-a505-2610679d88f5" alt="imagen-formulario" title="imagen-formulario"/>
+            <img className="imagengrande" src="https://firebasestorage.googleapis.com/v0/b/trabajosenmendoza-9801f.appspot.com/o/f2.png?alt=media&token=6629a1a2-ed13-40f9-a505-2610679d88f5" alt="imagen-formulario" title="imagen-formulario"/>
             </Link>
             </div>
 
             <div className=" centrar-textos">
             <Link target="_blank" to="https://igcoc9io.forms.app/formulario-de-cvs">
-            <img className="imagengrande" src="https://firebasestorage.googleapis.com/v0/b/trabajosenmendoza-9801f.appspot.com/o/f2.png?alt=media&token=a557f33d-7565-4893-a76d-f564145c04ed" alt="imagen-formulario" title="imagen-formulario"/>
+            <img className="imagengrande" src="https://firebasestorage.googleapis.com/v0/b/trabajosenmendoza-9801f.appspot.com/o/f1.png?alt=media&token=a557f33d-7565-4893-a76d-f564145c04ed" alt="imagen-formulario" title="imagen-formulario"/>
             </Link>
             </div>
 
